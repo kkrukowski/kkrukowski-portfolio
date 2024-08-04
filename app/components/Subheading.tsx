758df79ae9f8049273ad93b0ca108e810e8e512e
Kamil Krukowski <hadekshd@gmail.com>
2024-08-04 19:49:55 +0200

    feat: creating responsive design

--- app/components/Subheading.tsx
+++ app/components/Subheading.tsx
@@ -1,7 +1,7 @@
-export default function Subheading({ text, className}) {
+export default function Subheading({ text }) {
     return (
         <div>
-            <h2 className={`text-2xl font-medium ${className}`}>{text}</h2>
+            <h2 className={`text-2xl font-medium`}>{text}</h2>
         </div>
     )
 }
